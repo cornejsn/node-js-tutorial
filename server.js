fa6ad594ec0c0c17ffd0093383d44bbca01a08f4
cornejsn <cornejsn@gmail.com>
2021-07-26 12:11:24 -0400

    palindrome checker

--- server.js
+++ server.js
@@ -1,10 +1,45 @@
-const express = require('express'); //Line 1
-const app = express(); //Line 2
-const port = process.env.PORT || 5000; //Line 3
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+const http=require("http");
 
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/',function(req,res){
+    res.send('Homepage here');
+    var url="http://localhost:8080";
+    var req = http.request(url,res=>{
+        res.on('data', (chunk) => {
+        console.log(`BODY: ${chunk}`);
+        });
+    });
+    req.end();
 })
+
+app.get('/test',(req,res)=>{
+    var response=req.query;
+    var word = response.word;
+    console.log(response);
+    res.send(response);
+
+    checkPalindrome(word);
+});
+
+function checkPalindrome(str) {
+    // convert string to an array
+    const arrayValues = str.split('');
+
+    // reverse the array values
+    const reverseArrayValues = arrayValues.reverse();
+
+    // convert array to string
+    const reverseString = reverseArrayValues.join('');
+
+    if(str == reverseString) {
+        console.log('The word '+ str + ' is a palindrome.');
+    }
+    else {
+        console.log(str + 'is a palindrome');
+    }
+}
